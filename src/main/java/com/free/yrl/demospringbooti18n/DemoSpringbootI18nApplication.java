4ee71d6d7609d155cf3c0ac7afb27050491f83b8
YaoRenLiang <YaoRenLiang@foxmail.com>
2020-04-22 13:14:30 +0800

    项目构建

---
+++ src/main/java/com/free/yrl/demospringbooti18n/DemoSpringbootI18nApplication.java
@@ -0,0 +1,13 @@
+package com.free.yrl.demospringbooti18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootI18nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootI18nApplication.class, args);
+	}
+
+}
