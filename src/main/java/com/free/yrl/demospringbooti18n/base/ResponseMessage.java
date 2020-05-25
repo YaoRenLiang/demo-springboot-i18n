a265741238be69efe65ba6bc037c7de219f7052f
YaoRenLiang <YaoRenLiang@foxmail.com>
2020-05-25 17:01:08 +0800

    yrl

--- src/main/java/com/free/yrl/demospringbooti18n/base/ResponseMessage.java
+++ src/main/java/com/free/yrl/demospringbooti18n/base/ResponseMessage.java
@@ -14,9 +14,9 @@ import java.util.Map;
 import static org.springframework.util.ObjectUtils.isEmpty;
 
 /**
- * @author: 姚壬亮
- * @description: 基础的Controller
- * @date: 2019-10-11 10:01
+ * 响应结构体
+ *
+ * @author 姚壬亮
  **/
 @Getter
 @ApiModel(value = "response", description = "响应结果")
@@ -29,58 +29,116 @@ public class ResponseMessage<T> implements Serializable {
 	private String message;
 
 	@ApiModelProperty(value = "响应数据")
-	private T data;
+	private final T data;
 
 	private ResponseMessage(Integer code, String message, T data) {
 		this.code = code;
 		this.message = message;
 		this.data = data;
 		if (isEmpty(code)) {
-			this.code = ResponseMessageConstants.SERVICEEXCEPTION.getKey();
+			this.code = ResponseMessageConstants.SERVICE_EXCEPTION.getKey();
 		}
 		if (isEmpty(message)) {
-			this.message = ResponseMessageConstants.SERVICEEXCEPTION.getValue();
+			this.message = ResponseMessageConstants.SERVICE_EXCEPTION.getValue();
 		}
 	}
 
-	/*返回错误*/
+	/**
+	 * 根据状态码返回
+	 *
+	 * @param resultCode 结果状态码
+	 * @return 状态码
+	 */
+	public static <T> ResponseMessage<T> byCode(Integer resultCode) {
+
+		if (!resultCode.equals(ResponseMessageConstants.SUCCESS_OPERATION.getKey())) {
+			return error(resultCode);
+		}
+		return success();
+
+	}
+
+	/**
+	 * 根据数据处理成功的条数来返回
+	 *
+	 * @param successRow 成功的行数（例如：c u d操作）
+	 * @return 状态码
+	 */
+	public static Integer byRow(int successRow) {
+
+		if (successRow <= 0) {
+			return ResponseMessageConstants.SERVICE_EXCEPTION.getKey();
+		}
+		return ResponseMessageConstants.SUCCESS_OPERATION.getKey();
+
+	}
+
+	/**
+	 * 返回错误
+	 *
+	 * @param constants 枚举
+	 * @param <T>       范性
+	 * @return 返回体
+	 */
 	public static <T> ResponseMessage<T> error(ResponseMessageConstants constants) {
+
 		Integer key = constants.getKey();
-		return new ResponseMessage(key, msg(key), null);
+		return error(key);
+
 	}
 
-	/*返回错误*/
+	/**
+	 * 返回错误
+	 *
+	 * @param code 状态码
+	 * @param <T>  范性
+	 * @return 返回体
+	 */
 	public static <T> ResponseMessage<T> error(Integer code) {
-		return new ResponseMessage(code, msg(code), null);
+
+		return new ResponseMessage<>(code, msg(code), null);
+
 	}
 
-	/*返回错误*/
-	public static <T> ResponseMessage<T> error(Integer code, String message) {
-		return new ResponseMessage(code, msg(code, message), null);
-	}
-
-	/*返回成功*/
+	/**
+	 * 返回成功
+	 *
+	 * @param <T> 范性
+	 * @return 返回体
+	 */
 	public static <T> ResponseMessage<T> success() {
-		Integer key = ResponseMessageConstants.SUCCESSFULOPERATION.getKey();
-		return new ResponseMessage(key, msg(key), null);
+
+		Integer key = ResponseMessageConstants.SUCCESS_OPERATION.getKey();
+		return new ResponseMessage<>(key, msg(key), null);
+
 	}
 
-	/*返回成功*/
+	/**
+	 * 返回成功
+	 *
+	 * @param data 需要返回的数据
+	 * @param <T>  范性
+	 * @return 返回体
+	 */
 	public static <T> ResponseMessage<T> success(Object data) {
-		Integer key = ResponseMessageConstants.SUCCESSFULOPERATION.getKey();
+
+		Integer key = ResponseMessageConstants.SUCCESS_OPERATION.getKey();
 		if (data instanceof String || data instanceof Integer || data instanceof Date) {
-			Map map = Maps.newHashMap();
+			Map<String, Object> map = Maps.newHashMap();
 			map.put("result", data);
-			return new ResponseMessage(key, msg(key), map);
+			return new ResponseMessage<>(key, msg(key), (T) map);
 		}
 		if (isEmpty(data)) {
 			data = null;
 		}
-		return new ResponseMessage(key, msg(key), data);
+		return new ResponseMessage<>(key, msg(key), (T) data);
+
 	}
 
 	private static String msg(Integer key, Object... params) {
+
 		return Resources.getMessage("CODE_" + key, params);
+
 	}
 
 }
